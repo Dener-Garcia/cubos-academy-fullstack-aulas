006b36bad087e0e3672e1e1cc31151a51b2b12f1
Dener <dener.criarbr@gmail.com>
2024-02-10 12:11:37 -0300

    feat: solve exercise 11 e 12

--- 11/index.js
+++ 11/index.js
@@ -5,4 +5,16 @@ const rendaMensalEmCentavos = 300000;
 const mesesDecorridos = 12;
 
 // Soma das parcelas já pagas pelo aluno nos meses anteriores (em centavos). Se for igual a 18 mil reais, o aluno não deve pagar mais nada, pois já quitou a dívida.
-const totalJaPagoPeloAluno = 1000000;
+const totalJaPagoPeloAluno = 100000;
+
+let valorParcela = 0
+
+if (rendaMensalEmCentavos > 200000 && totalJaPagoPeloAluno < 1800000 ){
+    valorParcela = rendaMensalEmCentavos * 0.18
+    console.log("Valor da Parcela é R$",valorParcela/100)
+
+} else if(totalJaPagoPeloAluno = 1800000) {
+    console.log("Parabéns seu curso esta quitado")
+} else if(mesesDecorridos > 60) {
+    console.log("Parabéns seu curso esta quitado pois ja se passaram 60 meses")
+}
--- 12/index.js
+++ 12/index.js
@@ -1,1 +1,27 @@
 const diaDaSemana = 3;
+
+switch (diaDaSemana) {
+    case 1:
+        console.log("hoje é Segunda")
+    break;
+    case 2:
+        console.log("hoje é Terça")
+    break;
+    case 3:
+        console.log("hoje é Quarta")
+    break;
+    case 4:
+        console.log("hoje é Quinta")
+    break;
+    case 5:
+        console.log("hoje é Sexta")
+    break;
+    case 6:
+        console.log("hoje é Sabado")
+    break;
+    case 7:
+        console.log("hoje é Domingo")
+    break;
+    default:
+        console.log("Esse dia não existe, tente um número de 1 a 7")
+}
