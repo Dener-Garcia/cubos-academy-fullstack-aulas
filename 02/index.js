cc9a31b088cebf24bd6b555b99c6f3dcf04ef3d9
Dener Garcia <107416682+Dener-Garcia@users.noreply.github.com>
2024-02-15 21:21:31 -0300

    solve exercise 02

--- 02/index.js
+++ 02/index.js
@@ -1,1 +1,23 @@
-const letras = ["A", "a", "B", "C", "E", "e"];
+const letras = [ "A", "a", "B", "C", "E", "e"];
+
+letraDesejada = "e"
+let i = 0
+let quantidade = 0
+
+while (i < letras.length){
+    i++
+    console.log(letras[i])
+    if (letras[i] === letraDesejada.toLowerCase()){
+        console.log("achei a letra minuscula:", letraDesejada)
+        quantidade++
+    }else if (letras[i] === letraDesejada.toUpperCase()){
+        console.log("achei a letra maiuscula:", letraDesejada.toUpperCase())
+        quantidade++
+    } 
+}
+
+if (quantidade == 0 ){
+  console.log("Essa letra", letraDesejada, "nao esta no array")
+} else{
+  console.log("encontrei", quantidade, "letras:", letraDesejada, "na sua busca" )
+}
