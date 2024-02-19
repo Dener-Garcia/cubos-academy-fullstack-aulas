e7c57bcb5f7837713098c554a99bc5498392c639
Dener Garcia <107416682+Dener-Garcia@users.noreply.github.com>
2024-02-18 21:58:56 -0300

    Update index.js
    solve exercise 06

--- 06/index.js
+++ 06/index.js
@@ -1,2 +1,14 @@
 const numeros = [3, 4, 7, 8, 1, 6, 5, 10];
+let somaPares = 0
 
+let index = 0
+
+while(index < numeros.length){
+  const par = numeros[index] % 2
+  if (par === 0){
+    somaPares += numeros[index]
+  }
+  index ++
+}
+
+    console.log(somaPares)
