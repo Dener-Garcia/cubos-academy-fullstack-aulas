7cd77219dcb1dec38dfc8c974182677119e25ca9
Dener Garcia <107416682+Dener-Garcia@users.noreply.github.com>
2024-02-18 21:50:57 -0300

    Update index.js
    solve exercise 05

--- 05/index.js
+++ 05/index.js
@@ -1,1 +1,17 @@
 const original = [1, 4, 12, 21, 53, 88, 112];
+const newArray = []
+
+const arrayFinal = original.map((x) => x % 10)
+
+let index = 0
+
+while (index < original.length){
+  const par = original[index] % 2
+  if (par === 0){
+    newArray.push(original[index])
+  }
+  index ++
+}
+
+console.log(newArray)
+
