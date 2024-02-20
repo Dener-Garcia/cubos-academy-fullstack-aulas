55c031c670dff16183c36174e23666e6b300be99
Dener <dener.criarbr@gmail.com>
2024-02-20 01:08:43 -0300

    feat: solve exercises 07 08 and 09

--- 08/index.js
+++ 08/index.js
@@ -1,1 +1,19 @@
-const numeros = [3, 24, 1, 8, 11, 7, 15];
+const numeros = [3, 24, 1, 8, 11, 7, 215];
+
+let i = 0
+
+let bigNumber = 0
+
+while(i < numeros.length){
+    
+    if (bigNumber < numeros[i]){
+
+        bigNumber = numeros[i]
+    }
+
+    i++
+}
+console.log("O maior numero do array é", bigNumber)
+
+
+
