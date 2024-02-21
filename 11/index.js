5578e61c149754f13fd8d27c6c49184bd769d1d0
Dener Garcia <107416682+Dener-Garcia@users.noreply.github.com>
2024-02-20 21:41:47 -0300

    Update index.js
    solve exercise 11

--- 11/index.js
+++ 11/index.js
@@ -1,2 +1,19 @@
+
 const original = [5, 7, 13, 17, 26, 34, 118, 245];
+
+ let maiorDiferenca = 0;
+    
+    for (let i = 0; i < original.length; i++) {
+        for (let j = i + 1; j < original.length; j++) {
+
+            const diferenca = original[i] - original[j];
+            if (diferenca > maiorDiferenca) {
+                maiorDiferenca = diferenca;
+            } else if (-diferenca > maiorDiferenca) {
+                maiorDiferenca =- diferenca;
+            }
+        }
+    }
+
+console.log(maiorDiferenca)
 
