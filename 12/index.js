55085df3654651a41a31acfb8995a52e5541fa56
Dener Garcia <107416682+Dener-Garcia@users.noreply.github.com>
2024-02-20 21:59:29 -0300

    solve exercise 11

--- 12/index.js
+++ 12/index.js
@@ -1,3 +1,16 @@
+
 const filaDeDentro = ["Jose", "Maria", "Joao"];
 const filaDeFora = ["Joana", "Roberta", "Marcos", "Felipe"];
+
+let maxPessoasDentro = 6
+
+while (filaDeDentro.length < maxPessoasDentro){
+  const proximo =  filaDeFora.shift([maxPessoasDentro])
+  filaDeDentro.push(proximo)
+    console.log(proximo, filaDeDentro.length)
+    maxPessoasDentro--
+}
+
+console.log("Pessoas Dentro Banco",filaDeDentro)
+console.log("Pessoas Fora do bacno",filaDeFora)
 
