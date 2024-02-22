8ae99fc9599e1e45aa7ed24ae5ab2d1d0a3dc517
Dener <dener.criarbr@gmail.com>
2024-02-22 10:55:00 -0300

    feat: solve exercise 15

--- 13/index.js
+++ 13/index.js
@@ -1,2 +1,11 @@
 const disjuntores = [false, false, true, false, false, true, false, false];
 
+let i = 0
+
+while(i < disjuntores.length){
+    
+    if (disjuntores[i]){
+        console.log( "Os disjuntores, ", i, "tem seu estado em:",disjuntores[i])
+    }
+    i++
+}
--- 15/index.js
+++ 15/index.js
@@ -2,3 +2,19 @@ const original = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const pares = [];
 const impares = [];
 
+let i = 0
+
+while (original.length > i){
+    const numPar = original[i] % 2
+    if (numPar == 0){
+        pares.push(original[i])
+        console.log(original[i], "numeros pares")
+    } else{
+        impares.push(original[i])
+        console.log(original[i], "numeros impar")
+    }
+    i++
+}
+
+console.log(pares)
+console.log(impares)
