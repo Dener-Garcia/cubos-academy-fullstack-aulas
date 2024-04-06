283b885429768d2b26a48510b47e249a4c2e2de4
Dener Garcia <107416682+Dener-Garcia@users.noreply.github.com>
2024-04-06 00:46:32 +0000

    feat: create template express for api rest

--- src/index.js
+++ src/index.js
@@ -1,43 +1,11 @@
 const express = require("express")
+const routesGet = require("./routes")
+
 const app = express()
+// diz para node que as rotas recebem arquivos em .json
+app.use(express.json())
 
-const searchTeacher = require("./controllers/teacher-search")
-const teacherById = require("./controllers/teacherById")
-
-// rota padrao do localhost
-app.get("/", (require, response) => {
-    response.send("Meu servidor em nodeJS")
-})
-
-// criando um middleware indepedente, quando ele termina o codigo continua normalmente
-app.use((require, response, next) => {
-    console.log("Passei num middleware antes de chegar na rota")
-    next()
-})
-
-// rota /teacher retorna um objeto
-app.get("/teacher", (require, response) => {
-
-const teacherName = [{
-    teacher1: "dener",
-    teacher2: "Garcia",
-    teacher3: "Mara"
-}]
-    response.send(teacherName)
-})
-
-// criando um middleware de rota, quando ele termina o codigo continua normalmente, como boa pratica podemos usar com função tambem
-const middlewareRoute = (require, response, next) => {
-    console.log("Passei num middleware de rota")
-    // o next serve para executar a proxima acao apos o middlware, no caso a funcao teacherById
-    next()
-}
-
-// rota /teacher/:"any" com parametro de rota, serve para pegar o valor apos o /: da barra de endereco do browser, tambem adicionei um middleware como argumento na rota.
-app.get("/teacher/:id", middlewareRoute, teacherById )
-
-// rota /search-teacher com parametros de consulta, sao enviados pelo browser e recuperados dentro do require
-app.get("/search-teacher", searchTeacher)
+app.use(routesGet)
 
 
-app.listen(3300, console.log("servidor rodando na porta 3300"))
+app.listen(3000, console.log("server run at 3000 port"))
---
+++ status.md
@@ -0,0 +1,4 @@
+# Codigos de respostas para servidor
+
+## Status 200
+- 200 = 
